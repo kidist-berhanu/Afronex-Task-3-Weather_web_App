cffb048cc4ae59b70ed6a89a514058356c9453f1
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-04-26 16:51:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
 
-const key = "3c13867a590b54fd8a73772048323027";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const Key ="3c13867a590b54fd8a73772048323027";
+const Url ="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
 const cityName = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
-async function checkCurrentWeather(city){
-    const result = await fetch(apiUrl + city +`&appid = ${key}`);
+async function checkWeather(city){
+    const result = await fetch(Url + city + `&appid=${Key}`);
+
     if(result.status == 404){
         document.querySelector(".invalid").style.display = "block";
         document.querySelector(".weather").style.display = "none";
     }
-    var data = await result.json();
+    else{
+        var data = await result.json();
 
-    console.log(data);
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temprature").innerHTML =Math.round(data.main.temp)  + "°c";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temprature").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + "km/hr";
+        switch(data.weather[0].main) {
+            case "Clouds":
+                weatherIcon.src = "images/clouds.png";
+                break;
+            case "Clear":
+                weatherIcon.src = "images/day.svg";
+                break;
+            case "Rain":
+                weatherIcon.src = "images/rainy.svg";
+                break;
+            case "Drizzle":
+                weatherIcon.src = "images/drizzle.png";
+                break;
+            case "Mist":
+                weatherIcon.src = "images/mist.png";
+                break;
+            default:
+                break;
+        }        
 
-    switch(data.weather[0].main) {
-        case "Rain":
-            weatherIcon.src = "images/rain.png";
-            break;
-        case "Clear":
-            weatherIcon.src = "images/clear.png";
-            break;
-        case "Clouds":
-            weatherIcon.src = "images/clouds.png";
-            break;
-        case "Drizzle":
-            weatherIcon.src = "images/drizzle.png";
-            break;
-        case "Mist":
-            weatherIcon.src = "images/mist.png";
-            break;
-        case "Snow":
-            weatherIcon.src = "images/snow.png";
-            break;
-        default:
-            // Default case in case none of the above conditions are met
-            break;
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".invalid").style.display = "none";
     }
-    
 }
-searchBtn.addEventListener("click",()=>{
-    checkCurrentWeather(cityName.value);
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(cityName.value);
 })
+
